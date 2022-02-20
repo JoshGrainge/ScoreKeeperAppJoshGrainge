ad0a96d8785850284d175d9d6b3505b0e21b9b72
Josh Grainge <josh.grainge@hotmail.com>
2022-02-20 15:10:14 -0500

    Added documentation to code behind

--- app/src/main/java/com/example/scorekeeperappjoshgrainge/MainActivity.java
+++ app/src/main/java/com/example/scorekeeperappjoshgrainge/MainActivity.java
@@ -29,10 +29,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         redScoreText = (TextView) findViewById(R.id.red_team_score_text);
         blueScoreText = (TextView) findViewById(R.id.blue_team_score_text);
 
+        // Set spinner values to be score increment values in strings
         Spinner spinner = findViewById(R.id.spinner_score_interval);
         ArrayAdapter<CharSequence>adapter = ArrayAdapter.createFromResource(this,
                 R.array.score_increment, android.R.layout.simple_spinner_item);
-
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
@@ -43,27 +43,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bButton.setOnClickListener(this);
     }
 
+    // Add score on click
     @Override
     public void onClick(View view) {
 
         int redButtonId = R.id.add_red_score_button;
 
+        // Add to team that is associated with button
         if(view.getId() == redButtonId)
             AddRedScore();
         else
             AddBlueScore();
     }
 
+    // Add point value to red team score
     void AddRedScore() {
         redScore += GetIncrementValue();
         redScoreText.setText(String.valueOf(redScore));
     }
 
+    // Add point value to blue team score
     void AddBlueScore() {
         blueScore += GetIncrementValue();
         blueScoreText.setText(String.valueOf(blueScore));
     }
 
+    // Get the currently selected spinner value as an integer
     int GetIncrementValue() {
         Spinner spinner = findViewById(R.id.spinner_score_interval);
         return Integer.parseInt(spinner.getSelectedItem().toString());
